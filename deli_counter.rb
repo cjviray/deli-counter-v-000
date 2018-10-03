bd8bd8cf45668db177e056da40ad88a531c269af
Christian V <viray.christianj@gmail.com>
2018-10-03 07:41:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
  def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
  def now_serving(katz_deli)
