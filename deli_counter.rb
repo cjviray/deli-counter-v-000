c68ef01f4424cc4bde1a04a6ff9fff1314954105
Christian V <viray.christianj@gmail.com>
2018-10-03 07:31:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
-def line(array)
-  if array.count == 0
+def line(katz_deli)
+  if katz_deli.count == 0
     puts "The line is currently empty."
   else 
     status = "Current position: "
